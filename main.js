a5041f6c5b6caabd8c574e6124e65dd492e1f8bf
Heather Faerber <heatherdfaerber@gmail.com>
2019-12-12 13:50:53 -0700

    Add functionality to display winner card

--- main.js
+++ main.js
@@ -55,20 +55,58 @@ const validGuessWithinRange = (guess, display) => {
 }
 
 const displayNamesAndGuesses = () => {
-  $('#score-card-name-one').text($('#name-one-input').val())
-  $('#score-card-name-two').text($('#name-two-input').val())
+  $('.player-one-name').text($('#name-one-input').val())
+  $('.player-two-name').text($('#name-two-input').val())
   $('.challenger-one-guess').text($('#guess-one-input').val())
   $('.challenger-two-guess').text($('#guess-two-input').val())
 }
 
-const giveGuessFeedback = (guess, display) => {
+const giveGuessFeedback = (guess, display, player) => {
   if ($(guess).val() > randomNumber) {
     $(display).text("that's too high")
   } else if ($(guess).val() < randomNumber) {
     $(display).text("that's too low")
   } else {
     $(display).text("that's correct!")
+    addWinnerCard();
   }
+}
+
+const addWinnerCard = (player) => {
+  $('#right-column').prepend(`<article class="game-box" id="winner-card">
+    <div class="winner-card-top">
+      <span class="winner-card-top-span">
+        <h5 class="score-card-name-one vs-names player-one-name">CHALLENGER 1 NAME</h5>
+      </span>
+      <p class="vs">vs</p>
+      <span class="winner-card-top-span">
+        <h5 class="score-card-name-two vs-names player-two-name">CHALLENGER 2 NAME</h5>
+      </span>
+    </div>
+    <div class="winner-card-middle-box">
+      <div class="winner-card-middle-text-box">
+        <span class="winner-card-name">
+          <h3 class="winner-card-name"
+            id="winner-card-name">CHALLENGER 2 NAME</h3>
+        </span>
+        <h3>WINNER</h3>
+      </div>
+    </div>
+    <div class="winner-card-bottom">
+      <div class="bottom-item">
+        <span class="winner-card-stats">47</span>
+        <h5 class="lighter-h">GUESSES</h5>
+      </div>
+      <div class="bottom-item">
+        <span class="winner-card-stats">1.35</span>
+        <h5 class="lighter-h">MINUTES</h5>
+      </div>
+      <div class="bottom-item">
+        <button class="x-button" type="button" name="x"
+          value="x">x</button>
+      </div>
+    </div>
+  </article>`)
 }
 
 const requireCompletedInputs = () => {
